cc9e5ecae9aa0ec72f1b1afcba48e961bee393f9
Aleksandar <aleksandar.23@hotmail.com>
2021-11-26 23:26:04 +0100

    Small docker compose update

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const app = express()
 const PORT = 3000
+
 app.get('/', (req, res) => {
-  res.send("Hi from Docker again and again")
+  res.send("This is the index page")
+})
+
+app.get('/test', (req, res) => {
+  res.send("Again, change this part of the code")
 })
 
 app.listen(PORT,() => console.log(`Running on port ${PORT}`));
