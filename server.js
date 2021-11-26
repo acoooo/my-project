174060fd71a99f8c7e0e880ae5e87681faf4b673
Aleksandar <aleksandar.23@hotmail.com>
2021-11-26 19:15:13 +0100

    Adding docker compose file

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const PORT = 3000
 app.get('/', (req, res) => {
-  res.send("Hi")
+  res.send("Hi from Docker again and again")
 })
 
 app.listen(PORT,() => console.log(`Running on port ${PORT}`));
