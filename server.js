055d2d72791ee347ce1d94afe58274677dd3a61c
Aleksandar Trivic <aleksandar.23@hotmail.com>
2022-01-19 20:26:54 +0100

    Text change

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const app = express()
 const PORT = 3000
 
 app.get('/', (req, res) => {
-  res.send("Home page")
+  res.send("Home page from Docker")
 })
 
 app.get('/test', (req, res) => {
-  res.send("Test page")
+  res.send("Test page from Docker")
 })
 
 app.listen(PORT,() => console.log(`Running on port ${PORT}`));
